28b4eab4a6e7b0a02379af12096bf4027c1124e0
Riangalicha123 <130555814+Riangalicha123@users.noreply.github.com>
2024-04-02 18:40:36 +0800

    .

--- app/Views/Hotell/index.php
+++ app/Views/Hotell/index.php
@@ -39,62 +39,52 @@
         </div>
       </div>
     </section>
-    <!-- END section -->
-
-    <section class="site-section" style="background: linear-gradient(to bottom right,#F4E869, #3085C3, #FAF2D3,  #5CD2E6);">
+    <section class="site-section" style="background-image: url(/guest/images/malabomahalta.jpg); background-repeat: no-repeat; background-size: cover;">
       <div class="container">
         <div class="row align-items-center">
           <div class="col-md-4">
-            <div class="heading-wrap text-center element-animate"><!-- 
-              <h4 class="sub-heading">Stay with our luxury rooms</h4> -->
+            <div class="heading-wrap text-center element-animate">
               <h1 >About Us</h1>
-
-              <p class="mb-5"style="color: #161A30; text-align: justify;font-size:16px;">The term <b><span style="color: blue;font-size:20px;">MAHALTA</span></b>
- was coined by Florante Villarica in the book Mindoro that was published in the year 1998. Mahalta from the three things that Mindoreños are proud of: the peaceloving indigenous Mangyans who offers a rich artistic heritage to the history of the pre-colonial Philippines; Mt. Halcon, the fourt highest mountain in the Philippines and earnss the reputation of being the most difficult mountain to climb in the country. Its rich vegetation contains rich fauna and flora including the critically endangered Mindoro bleeding heart. Tamaraw, a fierce Mindoro Dwarf Buffalo, it symbolizes Mindoro since it could only be found in this island and nowhere else in the world.</p>
-              <!-- <p><a href="" class="btn btn-primary btn-sm">More About Us</a></p> -->
-            </div>
-          </div>
-          <div class="col-md-1"></div>
-          <div class="col-md-7">
-      <div id="imageCarousel" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="/guest/images/pool.png" alt="Image 1" class="img-md-fluid">
-            </div>
-            <div class="carousel-item">
-                <img src="/guest/images/Pic/9.jpg" alt="Image 2" class="img-md-fluid">
-            </div>
-            <div class="carousel-item">
-                <img src="/guest/images/Pic/5.jpg" alt="Image 2" class="img-md-fluid">
-            </div>
-            <div class="carousel-item">
-                <img src="/guest/images/MahaltaPic/20.jpg" alt="Image 2" class="img-md-fluid">
-            </div>
-            <div class="carousel-item">
-                <img src="/guest/images/MahaltaPic/22.jpg" alt="Image 2" class="img-md-fluid">
-            </div>
-            <!-- Add more carousel items with different images as needed -->
-        </div>
-
-        <!-- Add navigation arrows -->
-        <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-</div>
-
-
+              <p class="mb-5"style="color: black; text-align: justify;font-size:17.05px;">The term <b><span style="color: blue;font-size:20px;">MAHALTA</span></b>
+                was coined by Florante Villarica in the book Mindoro that was published in the year 1998. Mahalta from the three things that Mindoreños are proud of: the peaceloving indigenous Mangyans who offers a rich artistic heritage to the history of the pre-colonial Philippines; Mt. Halcon, the fourt highest mountain in the Philippines and earnss the reputation of being the most difficult mountain to climb in the country. Its rich vegetation contains rich fauna and flora including the critically endangered Mindoro bleeding heart. Tamaraw, a fierce Mindoro Dwarf Buffalo, it symbolizes Mindoro since it could only be found in this island and nowhere else in the world.</p>
+                            </div>
+                          </div>
+                          <div class="col-md-1"></div>
+                          <div class="col-md-7">
+                      <div id="imageCarousel" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="/guest/images/pool.png" alt="Image 1" class="img-md-fluid">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/guest/images/Pic/9.jpg" alt="Image 2" class="img-md-fluid">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/guest/images/Pic/5.jpg" alt="Image 2" class="img-md-fluid">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/guest/images/MahaltaPic/20.jpg" alt="Image 2" class="img-md-fluid">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/guest/images/MahaltaPic/22.jpg" alt="Image 2" class="img-md-fluid">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
         </div>
       </div>
     </section>
     <!-- END section -->
 
-    <section class="site-section "style="background: linear-gradient(to  bottom left,#3085C3,  #3085C3, #FAF2D3,  #5CD2E6, #FFFBE9,#F4E869,#F4E869);">
+    <section class="site-section "style="background-image: url(/guest/images/malabomahalta.jpg); background-repeat: no-repeat; background-size: cover;">
       <div class="container">
         <div class="row mb-5">
           <div class="col-md-12 heading-wrap text-center">
@@ -166,7 +156,7 @@
       </div>
     </section>
 
-    <section class="site-section "style="background: linear-gradient(to bottom right,#F4E869,  #FAF2D3, #5CD2E6,#3085C3);" >
+    <section class="site-section "style="background-image: url(/guest/images/malabomahalta.jpg); background-repeat: no-repeat; background-size: cover;">
       <div class="container">
         <div class="row mb-5">
           <div class="col-md-12 heading-wrap text-center">
@@ -218,7 +208,7 @@
       </div>
     </section>
 
-    <section class="site-section "style="background: linear-gradient(to  bottom left,#3085C3,#5CD2E6, #FAF2D3,  #FFFBE9,#F4E869,#F4E869);">
+    <section class="site-section "style="background-image: url(/guest/images/malabomahalta.jpg); background-repeat: no-repeat; background-size: cover;">
       <div class="container">
         <div class="row mb-5">
           <div class="col-md-12 heading-wrap text-center">
