d0cfd314b4457dcd368712364c36a4954ed2b44f
Riangalicha123 <130555814+Riangalicha123@users.noreply.github.com>
2024-10-02 21:26:03 +0800

    Update news.php

--- app/Views/Hotell/inc/news.php
+++ app/Views/Hotell/inc/news.php
@@ -1,6 +1,3 @@
-<!-- Include Bootstrap CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
 <style>
 #loaderr {
     position: fixed;
@@ -75,9 +72,6 @@
     <u class="exit-button" onclick="hideLoader()">Don't show</u>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 <script>
 document.addEventListener("DOMContentLoaded", function(event) { 
