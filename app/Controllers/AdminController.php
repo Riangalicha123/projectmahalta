b1223670175fad1e870aa5fc93f6f9b747d20839
Riangalicha123 <130555814+Riangalicha123@users.noreply.github.com>
2024-10-03 17:24:35 +0800

    Update AdminController.php

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -801,31 +801,13 @@ class AdminController extends BaseController
         $data = [
             'adminRoutes' => 'restReservation',
             'restrevs' => $this->reservation
-                ->select('
-                    reservations.ReservationID,
-                    restaurant_venue.VenueID,
-                    restaurant_venue.VenueName,
-                    reservations.ArivalDate,
-                    reservations.ArivalTime,
-                    reservations.CheckInDate,
-                    reservations.NumberOfGuests,
-                    IFNULL(reservations.Note, "NA") as Note, -- Set "NA" if Note is NULL
-                    reservations.Status,
-                    users.UserID,
-                    users.FirstName,
-                    users.LastName,
-                    users.ContactNumber,
-                    CONCAT(users.Region, ", ", users.Province, ", ", users.City, ", ", users.Barangay) as Address,
-                    reservations.UserID
-                ')
+                ->select('reservations.ReservationID, restaurant_venue.VenueID, restaurant_venue.VenueName, reservations.ArivalDate,reservations.ArivalTime, reservations.CheckInDate, reservations.NumberOfGuests, IFNULL(reservations.Note, "NA") as Note, reservations.Status, users.UserID,  users.FirstName, users.LastName, users.ContactNumber, CONCAT(users.Region, ", ", users.Province, ", ", users.City, ", ", users.Barangay) as Address, reservations.UserID ')
                 ->join('restaurant_venue', 'reservations.VenueID = restaurant_venue.VenueID')
                 ->join('users', 'reservations.UserID = users.UserID')
                 ->findAll()
         ];
-    
         return view('Admin/Restaurant/reservation', $data);
     }
-    
     public function addRestauReservation()
     {
         helper(['form']);
